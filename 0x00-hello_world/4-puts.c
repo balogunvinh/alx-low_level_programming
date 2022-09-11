43281d54bb6e962e3f6074e754d53d77e63959f8
balogunvinh <balogunvinh@gmail.com>
2022-09-10 18:49:14 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <std.io.h>
+
+/**
+ *
+ *   *main - Entry point
+ *
+ *   *Return: Always 0 (success)
+ *
+ *   */
+ 
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
